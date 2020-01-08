81218a44a233f44ab6f81efd27c72e8b5b4f435b
Edson <edson_csl@hotmail.com>
2020-01-08 20:38:10 -0300

    adding features to frontend and cors to backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes')
+const routes = require('./routes');
+const cors = require('cors')
 const app = express()
 app.use(express.json())
 mongoose.connect(`mongodb+srv://routeasy:123654789@routeasy-tuduk.mongodb.net/test?retryWrites=true&w=majority`, 
@@ -8,7 +9,7 @@ mongoose.connect(`mongodb+srv://routeasy:123654789@routeasy-tuduk.mongodb.net/te
     useNewUrlParser: true,
     useUnifiedTopology: true     
 })
-
+app.use(cors())
 app.use(routes);
 
 app.listen(4003);
--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,21 +1,101 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
 import './Form.css'
+import api from '../../services/api'
 export default function Form() {
+    const [name, setName] = useState('');
+    const [weight, setWeight] = useState('')
+    const [address, setAddress] = useState({})
+    const [latitude, setLatitude] = useState('')
+    const [longitude, setlongitude] = useState('')
+    async function handleClick(event){
+        event.preventDefault();
+         //const response = await api.post('/sessions', { email });
+         try {
+            const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(address)}&key=AIzaSyAJwTL_WQK7sIhlccPw7XhSLL_uoqlu_ic`)
+            console.log(response.data.results[0])
+            const lng = response.data.results[0].geometry.location.lng
+            const lat = response.data.results[0].geometry.location.lat
+            //const address = response.data.results[0];
+            const route = response.data.results[0]
+            setlongitude(lng)
+            setLatitude(lat)
+            setAddress( 
+                {
+                    numberStreet: route.address_components[0].long_name,
+                    street: route.address_components[1].long_name,      
+                    district: route.address_components[2].long_name,
+                    complement: '',
+                    city: route.address_components[3].long_name,
+                    state: route.address_components[4].short_name,
+                    country: route.address_components[5].short_name,
+                    geolocation: {
+                        lat: route.geometry.location.lat,
+                        lg:  route.geometry.location.lng
+                    }
+                } 
+            )
+         } catch (error) {
+             console.error(`Erro na requisição -> ${error}`)
+         } 
+      }
+    async function handleSubmit(event){
+        event.preventDefault();
+       
+         try {
+            /* setAddress({
+                "street": "Rua Alameda B",
+                "numberStreet": 225,
+                "district": "Jardins",
+                "complement": "Cond Regent Garden",
+                "city": "Aracaju",
+                "state": "Sergipe",
+                "country": "Brasil",
+                "geolocation": {
+                    "lat": "-2151.5",
+                    "lg": "+21651.4"
+                } )*/
+             await api.post('/delivery', {name, weight, address})
+          
+         } catch (error) {
+             console.error(`Erro ao salvar delivery -> ${error}`)
+         } 
+    }
+
     return (
         <div id="card">
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="Name">
-                    <input type="text" name="name" id="name" placeholder="Nome do Cliente"/>
+                    <input className="data" 
+                            type="text" 
+                            name="name" id="name" 
+                            required placeholder="Nome do Cliente"  
+                            onChange={event => setName(event.target.value)}/>
                 </label>
                 <label htmlFor="weight of Delivery">
-                    <input type="text" name="weight" id="weight" placeholder="Peso da Entrega"/>
+                    <input className="data" 
+                            type="text" 
+                            name="weight" 
+                            id="weight" 
+                            required placeholder="Peso da Entrega"
+                            onChange={event => setWeight(event.target.value)}/>
                 </label>
                 <label htmlFor="Address">
-                    <input type="text" name="address" id="weight" placeholder="Endereço Cliente"/>
-                    <button type="submit" className="btnSearch">Buscar</button>
+                    <input className="data" 
+                            type="text" name="address" 
+                            id="address" 
+                            required placeholder="Endereço Cliente"
+                            onChange={event => setAddress(event.target.value)}/>
+                    <button type="submit" className="btnSearch" onClick={handleClick}>Buscar</button>
                 </label>
-                <button type="submit" className="btn" style={{backgroundColor: '#3CBC8D', marginTop: '20px'}}>Cadastrar Cliente</button>
-                <button type="submit" className="btn"  style={{backgroundColor: '#e10000', marginTop: '15px'}}>Resetar Cadastro</button>
+                <label htmlFor="geo-long">
+                    <input className="geo" id="long" value={longitude}  type="text" name="long" disabled required placeholder="Longitude"/>
+                </label>
+                <label htmlFor="geo-lat">
+                    <input className="geo" type="text" value={latitude} name="lat" id="weight" disabled required placeholder="Latitude"/>
+                </label>
+                <button  className="btn" style={{backgroundColor: '#3CBC8D', marginTop: '20px'}}>Cadastrar Cliente</button>
+                <button  className="btn"  style={{backgroundColor: '#e10000', marginTop: '15px'}}>Resetar Cadastro</button>
             </form>
         </div>
     )
